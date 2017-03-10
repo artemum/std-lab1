c8662dd8a683791f358cd441256add843d5959cf
Artem Litvinenko <mazerfakerov@gmail.com>
2017-03-10 09:58:07 +0300

    ЭТО МОЙ ВТОРОЙ КОММИТ

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+     int a,b;
+    cout << "VVEDITE DVA CHESLA";
+    cin >> a >> b;
+    cout << "VI VVELI DVA CHESLA ";
+    return (0);
+ }
