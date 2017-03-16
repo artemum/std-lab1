1cbe4a7ff9ea5c5e105b8eae8709ae5fd1e1c07e
Artem Litvinenko <mazerfakerov@gmail.com>
2017-03-16 09:08:57 +0300

    ДОБАВИЛ В ПРОГУ  разность двух чисел

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main()
     cout << "VVEDITE DVA CHESLA : ";
     cin >> a >> b;
     cout << "SUMMA CHISEL " << a+b;
-
+    cout << " VVEDITE ESCHE DVA CHESLA  =) : ";
+    cin >> a >> b;
+    cout << "RAZNOST CHISEL"<< a-b;
     return (0);
  }
